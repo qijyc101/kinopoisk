d4af9ed59d97806ee2363f89b34ebdb5b8c5a099
Vadim Demchenko <qijyc101@vk.com>
2024-05-05 14:38:58 +0700

    chore: project setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import path from 'path';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 8080,
+  },
   plugins: [vue()],
-})
+});
